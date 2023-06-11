5f84d418b80bc1a00d12d66b78cd1a2d965c19b1
Wahyu Prabowo <wahyuprabowo601@gmail.com>
2023-06-11 20:40:11 +0700

    sudah di compile

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'waduh',
+  appName: 'TugasApp',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
